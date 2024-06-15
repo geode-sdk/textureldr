e94aa6f269eed3062b9274337c67e3ee1b82d8fb
matcool <26722564+matcool@users.noreply.github.com>
2024-06-14 21:43:13 -0300

    dont trigger fallback on icons, in a very stupid way

--- src/Fallback.cpp
+++ src/Fallback.cpp
@@ -38,6 +38,24 @@ class $modify(CCSpriteFrameCache) {
     cocos2d::CCSpriteFrame* spriteFrameByName(char const* name) {
         auto* frame = CCSpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName(name);
         if (frame == nullptr) {
+            // this is stupid but rob intentionally doesnt load all icons at startup,
+            // probably to save memory, so do this to not use fallback on icons
+            static constexpr std::string_view prefixes[] = {
+                "player_",
+                "ship_",
+                "dart_",
+                "bird_",
+                "robot_",
+                "spider_",
+                "swing_",
+                "jetpack_",
+            };
+            const std::string_view nameStr = name;
+            for (auto const& prefix : prefixes) {
+                if (nameStr.find(prefix) != -1) {
+                    return frame;
+                }
+            }
             frame = CCSpriteFrame::create("fallback.png"_spr, {ccp(0, 0), ccp(128, 128)});
         }
         return frame;
